b0c91729c09a8b5afa46633c514867bb96f7b51f
Audrey <66935319+AudreyLau8@users.noreply.github.com>
2021-04-23 22:58:55 -0700

    added clearscreens and finalized text
    fresh/transition scenes

--- main.py
+++ main.py
@@ -15,12 +15,23 @@ answer = screen.textinput("Ask a Question", "Or type quit to exit")
 
 bank = ["Not likely", "Hm, I'm not sure", "Why do you ask", "Yes", "No", "ABSOLUTELY NOT", "Ask again", "I guess"]
 
+def reset():
+  turtle.clearscreen()
+  turtle.hideturtle()
+  drawPig()
+
+num = 1
 while (answer != "quit"):
+  reset()
   turtle.write("You asked: " +answer, align="center", font=("Times New Roman",20, "bold"))
-  time.sleep(2)
+  time.sleep(3)
   reply=bank[randint(0,7)]
   turtle.write(reply, align="center", font=("Arial", 20, "bold"))
+  time.sleep(2)
+  answer = screen.textinput("Ask a Question", "Wait 3s for response or type quit to exit")
   
 
 if (answer == "quit"):
-  turtle.write("Thanks for playing!", align="center", font=("Times New Roman",10, "bold"))
+  turtle.clearscreen()
+  turtle.hideturtle()
+  turtle.write("I knew it. Bye-bye!", align="center", font=("Arial", 20, "bold"))
